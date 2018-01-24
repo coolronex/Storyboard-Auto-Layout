4f27cb8559d5f588ff02cc125272ad8b633726f5
Aaron <heyitsronex@gmail.com>
2018-01-23 19:39:29 -0500

    first commit

---
+++ Auto Layout Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auto Layout Storyboard
+//
+//  Created by Aaron Chong on 1/23/18.
+//  Copyright © 2018 Aaron Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Auto Layout Storyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Auto Layout Storyboard
+//
+//  Created by Aaron Chong on 1/23/18.
+//  Copyright © 2018 Aaron Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
